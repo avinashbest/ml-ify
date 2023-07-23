025d2970ca1db220b4de9b55d4f219544b0399f0
avinashbest <avinashbest3@gmail.com>
2023-07-23 23:59:07 +0530

    barcode scanner integration

---
+++ app/src/main/java/com/android/mlify/barcode/BarcodeActivity.kt
@@ -0,0 +1,92 @@
+package com.android.mlify.barcode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.TextView
+import com.android.mlify.R
+import com.android.mlify.databinding.ActivityBarcodeBinding
+import com.google.mlkit.common.MlKitException
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+import java.util.Locale
+
+class BarcodeActivity : AppCompatActivity() {
+
+
+    private var allowManualInput = false
+    private lateinit var binding: ActivityBarcodeBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityBarcodeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onAllowManualInputCheckboxClicked(view: View) {
+        allowManualInput = (view as CheckBox).isChecked
+    }
+
+    fun onScanButtonClicked(view: View) {
+        val optionsBuilder = GmsBarcodeScannerOptions.Builder()
+        if (allowManualInput) {
+            optionsBuilder.allowManualInput()
+        }
+        val gmsBarcodeScanner = GmsBarcodeScanning.getClient(this, optionsBuilder.build())
+        gmsBarcodeScanner
+            .startScan()
+            .addOnSuccessListener { barcode: Barcode ->
+                binding.barcodeResultView.text = getSuccessfulMessage(barcode)
+            }
+            .addOnFailureListener { e: Exception ->
+                binding.barcodeResultView.text = getErrorMessage(e)
+            }
+            .addOnCanceledListener {
+                binding.barcodeResultView.text = getString(R.string.error_scanner_cancelled)
+            }
+    }
+
+    override fun onSaveInstanceState(savedInstanceState: Bundle) {
+        savedInstanceState.putBoolean(KEY_ALLOW_MANUAL_INPUT, allowManualInput)
+        super.onSaveInstanceState(savedInstanceState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        allowManualInput = savedInstanceState.getBoolean(KEY_ALLOW_MANUAL_INPUT)
+    }
+
+    private fun getSuccessfulMessage(barcode: Barcode): String {
+        val barcodeValue =
+            String.format(
+                Locale.US,
+                "Display Value: %s\nRaw Value: %s\nFormat: %s\nValue Type: %s",
+                barcode.displayValue,
+                barcode.rawValue,
+                barcode.format,
+                barcode.valueType
+            )
+        return getString(R.string.barcode_result, barcodeValue)
+    }
+
+    private fun getErrorMessage(e: Exception): String? {
+        return if (e is MlKitException) {
+            when (e.errorCode) {
+                MlKitException.CODE_SCANNER_CAMERA_PERMISSION_NOT_GRANTED ->
+                    getString(R.string.error_camera_permission_not_granted)
+
+                MlKitException.CODE_SCANNER_APP_NAME_UNAVAILABLE ->
+                    getString(R.string.error_app_name_unavailable)
+
+                else -> getString(R.string.error_default_message, e)
+            }
+        } else {
+            e.message
+        }
+    }
+
+    companion object {
+        private const val KEY_ALLOW_MANUAL_INPUT = "allow_manual_input"
+    }
+}
